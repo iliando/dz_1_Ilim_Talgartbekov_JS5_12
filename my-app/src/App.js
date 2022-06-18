02e5cedad2dfcb7f5acadce206d10c0a1333162b
test <test1>
2022-06-18 17:26:36 +0600

    dz1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from "./pages/mainPage/MainPage";
+
+function App() {
+    return (
+        <div>
+            <MainPage/>
+        </div>
+    );
+}
+
+export default App;
